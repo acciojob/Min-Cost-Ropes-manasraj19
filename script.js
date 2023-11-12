5ba8ee3b5db3064696d522a8247ca4f5e150000b
Manas Raj <119037651+manasraj19@users.noreply.github.com>
2023-11-12 22:01:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+  const heap = [...arr];
+  heap.sort((a, b) => a - b);
+
+  let totalCost = 0;
+
+  while (heap.length > 1) {
+    const firstMin = heap.shift();
+    const secondMin = heap.shift();
+
+    const currentCost = firstMin + secondMin;
+    totalCost += currentCost;
+
+    heap.push(currentCost);
+    heap.sort((a, b) => a - b);
+  }
+
+  return totalCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
+
+// Examples
+console.log(mincost([4, 3, 2, 6]));  // Output: 29
+console.log(mincost([1, 2, 3, 4, 5]));  // Output: 33
